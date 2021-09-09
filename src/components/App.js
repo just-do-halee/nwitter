884e00d8fea58f9d43bcd9d3c3cf3f5547b7c4b4
just-do-halee <just.do.halee@gmail.com>
2021-09-09 23:51:10 +0900

    1.  router  [update]

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import AppRouter from './Router';
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+    </>
+  );
+}
+
+export default App;
